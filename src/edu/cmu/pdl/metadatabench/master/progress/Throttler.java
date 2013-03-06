4489c99967cf47a09320153c01689c90fceeeb11
emil.rakadjiev <e.rakadjiev@hotmail.com>
2013-03-06 19:37:29 +0100

    Minor change in log messages

--- src/edu/cmu/pdl/metadatabench/master/progress/Throttler.java
+++ src/edu/cmu/pdl/metadatabench/master/progress/Throttler.java
@@ -16,15 +16,16 @@ public class Throttler {
 	public static void throttle(int i){
 		if((i % THROTTLE_AFTER_GENERATED_OPS) == 0){
 			int waitForOps = i - THROTTLE_CONTINUE_THRESHOLD;
+			log.info("Pausing generation. Waiting for the so far generated operations to be executed.");
 			while(waitForOps >= ProgressBarrier.getOperationsDone()){
 				try {
-					log.debug("Waiting for the so far generated operations to be executed.");
 					log.debug("Going to sleep for {} ms", THROTTLE_DURATION);
 					Thread.sleep(THROTTLE_DURATION);
 				} catch (InterruptedException e) {
 					log.warn("Thread was interrupted while sleeping", e);
 				}
 			}
+			log.info("Continuing generation.");
 		}
 	}
 
